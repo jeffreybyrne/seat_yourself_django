78dde2c4473f56baa29f011e925df693a67164a0
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-13 16:55:41 -0400

    restricted reservations that are too close to closing time

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -55,8 +55,18 @@ class ReservationForm(ModelForm):
         cleaned_party_size = cleaned_data['party_size']
         restaurant = self.instance.restaurant
         reservation_datetime = datetime(cleaned_date.year, cleaned_date.month, cleaned_date.day, cleaned_time.hour, cleaned_time.minute)
+
         if reservation_datetime < (datetime.now() + timedelta(minutes=30)):
             self.add_error('time', 'Reservation must be at least 30 minutes in future')
+
+        if not restaurant.open_past_midnight():
+            if cleaned_time > restaurant.last_reservation_time:
+                self.add_error('time', 'Reservations are not available at that time.')
+        else:
+            if (restaurant.last_reservation_time > restaurant.closing_time) and (cleaned_time > restaurant.last_reservation_time):
+                self.add_error('time', 'Reservations are not available at that time.')
+            elif (restaurant.last_reservation_time < restaurant.closing_time) and (cleaned_time > restaurant.last_reservation_time) and (cleaned_time < restaurant.opening_time):
+                self.add_error('time', 'Reservations are not available at that time.')
 
         if not restaurant.room_for(cleaned_date, cleaned_time, cleaned_party_size):
             self.add_error('time', 'Restaurant is booked at that time')
@@ -66,6 +76,7 @@ class ReservationForm(ModelForm):
 class RestaurantForm(ModelForm):
     opening_time = TimeField(widget=TimeInput(attrs={'type': 'time' }))
     closing_time = TimeField(widget=TimeInput(attrs={'type': 'time' }))
+    last_reservation_time = TimeField(widget=TimeInput(attrs={'type': 'time' }))
     name = CharField()
     address = CharField()
     phone = CharField()
@@ -76,7 +87,7 @@ class RestaurantForm(ModelForm):
 
     class Meta:
         model = Restaurant
-        fields = ['name', 'description', 'address', 'phone', 'capacity', 'opening_time', 'closing_time', 'image', 'category']
+        fields = ['name', 'description', 'address', 'phone', 'capacity', 'opening_time', 'closing_time', 'last_reservation_time', 'image', 'category']
 
 class ProfileForm(ModelForm):
     first_name = CharField()
